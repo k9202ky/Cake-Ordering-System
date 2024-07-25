f47459dcdea95b6a9bc4b2bfc6eb47780544d8f9
k9202ky <k9202ky@gmail.com>
2024-07-26 01:41:30 +0800

    upload

--- app.js
+++ app.js
@@ -40,6 +40,11 @@ app.use(session({
   cookie: { secure: process.env.NODE_ENV === 'production', maxAge: 24 * 60 * 60 * 1000 } // 24 小時
 }));
 
+app.use(cors({  
+  origin: process.env.FRONTEND_URL,
+  credentials: true 
+})); 
+
 // 其他中間件設置
 app.use(logger('dev'));
 app.use(express.json());
